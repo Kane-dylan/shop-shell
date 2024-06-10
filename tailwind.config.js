0c4fdd96b8ea813ccffae2eaf34585349a32fa41
Kane-dylan <cool.kdtech1234@gmail.com>
2024-06-10 21:57:51 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,10 @@ module.exports = {
   content: ["./docs/**/*.{html,js}","node_modules/preline/dist/*.js"],
   theme: {
     extend: {
-      backgroundImage: {
-        'hero-pattern': "url('./asets/cool-background.svg')",
-        'footer-texture': "url('./asets/cool-background.png')",
-      }
     },
   },
   plugins: [require('@tailwindcss/forms'),require('preline/plugin')],
+  // darkMode: "class",
+
 }
 
